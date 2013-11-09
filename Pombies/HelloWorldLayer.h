75244f6a580637797381db85a2a67821d5d56266
Cristian Pereyra <cristian@redmintlabs.com>
2013-11-09 16:46:59 -0300

    Initial Commit

---
+++ Pombies/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  Pombies
+//
+//  Created by Cristian Pereyra on 11/9/13.
+//  Copyright Cristian Pereyra 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ Pombies/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  Pombies
+//
+//  Created by Cristian Pereyra on 11/9/13.
+//  Copyright Cristian Pereyra 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
